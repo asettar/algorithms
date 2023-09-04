106828c2c292eb42dc14cf8a00ac00b9ac82e61a
achraf <achrafsettar8@gmail.com>
2023-09-04 20:18:09 +0100

    Mon Sep  4 20:18:09 +01 2023

--- Trie/prb7.cpp
+++ Trie/prb7.cpp
@@ -7,6 +7,10 @@ using namespace std;
 // ● The function returns all the
 // current strings in the trie sorted
 
+// void auto_complete(const string &str, vector<string> &res)
+// ● Given a string, the function will make an auto-complete for it.
+// In other words, return all full words that are in trie and has this prefix
+
 class trie{
 	map<char, trie*> childs;
 	bool is_leaf = 0;
@@ -37,6 +41,25 @@ public:
 			node->get_all_paths(path);
 		}
 	}
+	void	get_words(trie *node, string str, vector<string> &res){
+		if (node->is_leaf)
+			res.push_back(str);
+		for(auto &p : node->childs){
+			node = p.second;
+			get_words(node, str+p.first, res);
+		}
+	}
+
+	void auto_complete(const string &str, vector<string> &res){
+		trie *node = this;
+		for(char c : str){
+			if (!node->childs.count(c))
+				return ;
+			node = node->childs[c];
+		}
+		get_words(node, str, res);
+	}
+
 };
 
 int	main()
@@ -44,15 +67,22 @@ int	main()
     trie tr;
 	tr.insert("abcd");
 	tr.insert("xyz");
+	tr.insert("abyz");
 	tr.insert("a");
+	tr.insert("abx");
 	tr.insert("ab");
 	tr.insert("xyzw");
 	tr.insert("bcd");
 	tr.insert("bcd");
 	vector<string> res;
 	tr.get_all_paths(res);
-	// sort(res.begin(), res.end());
 	for(string s: res){
 			cout << s << ' ';
 	}
+	cout << endl;
+	res.clear();
+	tr.auto_complete("ab", res);
+	for(string s : res)
+		cout << s << ' ';
+	cout << endl;
 }
