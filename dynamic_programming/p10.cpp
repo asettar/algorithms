585b06ee553f9c6a81864a1d47bce1b769745a01
Achraf Settar <achrafsettar8@gmail.com>
2023-10-22 00:24:58 +0100

    Sun Oct 22 00:24:58 +01 2023

--- dynamic_programming/p10.cpp
+++ dynamic_programming/p10.cpp
@@ -5,11 +5,25 @@
 using namespace std;
 
 
-// prbLink: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+// prbLink: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 
 class Solution {
+	int rec(int i, int buy, int fee, vector<int>&prices){
+		if (i == prices.size())
+			return 0;
+		int ans = INT_MIN;
+		if (buy){
+			ans = max(ans, rec(i + 1, buy, fee, prices));
+			ans = max(ans, -fee + prices[i] + rec(i + 1, 0, buy, prices));
+		}
+		else{
+			ans = max(ans, rec(i + 1, buy, fee, prices));
+			ans = max(ans, -prices[i] + rec(i + 1, 1, fee, prices));
+		}
+		return ans;
+	}
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        
+    int maxProfit(vector<int>& prices, int fee) {
+        return rec(0, 0, fee, prices);
     }
 };
