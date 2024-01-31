39e2beb5be9dfa3f867f55f3b261577bf037937f
achraf <achrafsettar8@gmail.com>
2024-01-31 19:53:46 +0100

    Wed Jan 31 19:53:46 +01 2024

--- Segment_tree/52C.cpp
+++ Segment_tree/52C.cpp
@@ -1,36 +1,19 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <sstream>
 #define int long long
 using namespace std;
- 
-// when updating a range we have a left and right,
-// when we coincide with a range that is fully insinde our (l, r) we stop
- 
-// update the node when you visited it, propagted down the update 
- 
-// range ubdate (l, r) + val: 
-	// traverse the seg tree
-	//update prev remaining update if ther is 
-	// check if the node completely overlap with (l, r)
-		// -update the node
-		// -lazy propagate to children if it has and return  (lazy[2*ind]+=val, lazy[2*ind+2]+=val)
- 
-	// check if the node partially overlap
-		// -go left , go right
-	// no overlap 
-		// -return (stop the traverse)
-// range query  : check if node need ubdate other are just similar to noraml segTree
- 
- 
+
+// prblink: https://codeforces.com/contest/52/problem/C
 struct segTree{
 	vector<int> all;
 	int n;
 	vector<int> s;
 	vector<int> lazy;
- 
+
 	int fn(int a, int b){
-		return a + b;
+		return min(a, b);
 	}
 	segTree(vector<int> &a){
 		n = a.size();
@@ -51,14 +34,14 @@ struct segTree{
 	}
 	
 	void	push_down(int node, int start, int end) {
-		s[node] += lazy[node] * (end - start + 1);
+		s[node] += lazy[node];
 		if (start != end){
 			lazy[2 * node] += lazy[node];
 			lazy[2 * node + 1] += lazy[node];
 		}
 		lazy[node] = 0;
 	}
- 
+
 	void	update(int node, int start, int end, int l, int r, int val){
 		push_down(node, start, end);
 		if (start > r ||  end < l) return ;
@@ -78,33 +61,45 @@ struct segTree{
 	
 	int query(int node, int start, int end, const int l, const int r){
 		push_down(node, start, end);
-		if (start > r || end < l) return 0;
+		if (start > r || end < l) return 2e18;
 		if (start >= l && end <= r) return s[node];
 		int mid = (start + end) / 2;
 		int left = query(2 * node, start, mid, l, r);
 		int right = query(2 * node + 1, mid + 1, end, l, r);
-		return left + right;
+		return min(left, right);
 	}
 	int query(int l, int r){
 		return query(1, 0, n - 1, l, r);
 	}
 };
- 
- 
+
 int32_t main(){
-	int n, q; cin >> n >> q;
-	vector <int> a(n);
+	int n; cin >> n;
+	vector<int> a(n);
 	for(int &i : a) cin >> i;
-	segTree s(a);
+	segTree seg(a);
+	int q; cin >> q; cin.ignore();
 	while(q--){
-		int type; cin >> type;
-		if (type == 1){
-			int a, b, u; cin >> a >> b >> u;
-			s.update(a - 1, b - 1, u);
+		string s;
+		getline(cin, s);
+		stringstream ss(s);
+		vector<int> nums;
+		int nn;
+		while (ss >> nn)
+			nums.push_back(nn);
+		int l = nums[0], r = nums[1], val;
+		if (nums.size() == 3){
+			val = nums[2];
+			if (l > r){
+				seg.update(l, n - 1, val);
+				seg.update(0, r, val);
+			}
+			else seg.update(l, r, val);
 		}
-		else {
-			int k; cin >> k;
-			cout << s.query(k - 1, k - 1) << endl;
+		else{
+			if (l > r)
+				cout << min(seg.query(l, n - 1), seg.query(0, r)) << endl;
+			else cout << seg.query(l, r) << endl;
 		}
 	}
 }
